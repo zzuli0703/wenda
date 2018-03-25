b6b2e2c9dd919d76b92457232846a5cd96ee5909
ff <11422qq.com>
2018-03-25 23:08:23 +0800

    增加评论功能

---
+++ src/main/java/com/nowcoder/service/CommentService.java
@@ -0,0 +1,31 @@
+package com.nowcoder.service;
+
+import com.nowcoder.dao.CommentDao;
+import com.nowcoder.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.xml.ws.Action;
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentDao commentDAO;
+
+    public List<Comment> getCommentsByEntity(int entityId, int entityType){
+        return commentDAO.selectByEntity(entityId, entityType);
+    }
+
+    public int addComment(Comment comment){
+        return commentDAO.addComment(comment);
+    }
+
+    public int getCommentCount(int entityId, int entityType){
+        return commentDAO.getCommentCount(entityId, entityType);
+    }
+
+    public void delectComment(int entityId, int entityType){
+        commentDAO.updateStatus(entityId, entityType, 1);
+    }
+}
--- src/main/java/com/nowcoder/util/WendaUtil.java
+++ src/main/java/com/nowcoder/util/WendaUtil.java
@@ -2,7 +2,7 @@ package com.nowcoder.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import com.alibaba.fastjson.JSONObject;
 import java.security.MessageDigest;
 import java.util.Map;
 
@@ -11,6 +11,30 @@ import java.util.Map;
  */
 public class WendaUtil {
     private static final Logger logger = LoggerFactory.getLogger(WendaUtil.class);
+
+    public static int ANONYMOUS_USERID=3;
+
+    public static String getJSONString(int code) {
+        JSONObject json = new JSONObject();
+        json.put("code", code);
+        return json.toJSONString();
+    }
+
+    public static String getJSONString(int code, String msg) {
+        JSONObject json = new JSONObject();
+        json.put("code", code);
+        json.put("msg", msg);
+        return json.toJSONString();
+    }
+
+    public static String getJSONString(int code, Map<String, Object> map) {
+        JSONObject json = new JSONObject();
+        json.put("code", code);
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            json.put(entry.getKey(), entry.getValue());
+        }
+        return json.toJSONString();
+    }
 
     public static String MD5(String key) {
         char hexDigits[] = {
